2faad08093b2176932f03583e17b1353aacdd5b4
Georg Lehner <jorge@air>
2025-12-29 21:03:11 +0100

    fLisp 0.14, copied over from Femto

---
+++ file.h
@@ -0,0 +1,15 @@
+#ifndef FILE_H
+#define FILE_H
+
+#include "flisp.h"
+
+extern void lisp_file_register(Interpreter *);
+
+#endif
+/*
+ * Local Variables:
+ * c-file-style: "k&r"
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
